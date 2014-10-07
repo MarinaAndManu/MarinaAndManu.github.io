fe68282eaf1363e7901cf41cadd80814378c3e73
manutero <manutero@gmail.com>
2014-10-07 20:32:10 +0200

    customizing mouse over js code.

--- js/menu_mouse_over.js
+++ js/menu_mouse_over.js
@@ -5,35 +5,19 @@
 var mouse_position;
 var animating = false;
 
-//GET MOUSE POSITION
 $(document).mousemove(function (e) {
-   //$("body").on("mousemove", function(mouse_pointer) {
-   //console.log(mouse_pointer.pageX - $(window).scrollLeft());
-   //mouse_position = mouse_pointer.pageX - $(window).scrollLeft();
-   if (animating) {
-       return;
-   }
+   if (animating) {return;}
    mouse_position = e.clientX;
 
-   // console.log(mouse_position);
    if (mouse_position <= 100) {
-       //SLIDE IN MENU
-       animating = true;
-       $('#cms_bar').animate({
-           left: 0,
-           opacity: 1
-       }, 200, function () {
-           animating = false;
-       });
-       // console.log('menu shown');
+     animating = true;
+     $('#cms_bar').animate({left: 0, opacity: 0.9}, 200, function () {
+        animating = false;
+      });
    } else if (mouse_position > 100) {
-       animating = true;
-       $('#cms_bar').animate({
-           left: -100,
-           opacity: 0
-       }, 500, function () {
-           animating = false;
+      animating = true;
+      $('#cms_bar').animate({left: -80, opacity: 0.7}, 500, function () {
+          animating = false;
        });
-       // console.log('menu hidden');
    }
 });
