5b370e4e2b86116b9b7aea218732db40aee0dd0e
manutero <manutero@gmail.com>
2014-10-09 23:05:28 +0200

    Centralized all the js code in one master file.

--- js/map.js
+++ js/map.js
@@ -120,3 +120,28 @@ function createPopUpHTML() {
   $newSlide.addClass('active').show();
   return false;
 }
+
+/**
+ * Menu mouse over position
+ * Stackoverflow: 19618103
+ * http://jsfiddle.net/ravikumaranantha/Wtfpx/2/
+ */
+var mouse_position;
+var animating = false;
+
+$(document).mousemove(function (e) {
+   if (animating) {return;}
+   mouse_position = e.clientX;
+
+   if (mouse_position <= 100) {
+     animating = true;
+     $('#cms_bar').animate({left: 0, opacity: 0.9}, 200, function () {
+        animating = false;
+      });
+   } else if (mouse_position > 100) {
+      animating = true;
+      $('#cms_bar').animate({left: -80, opacity: 0.7}, 500, function () {
+          animating = false;
+       });
+   }
+});
