9d242a6a9b2da3b3e99e0dc645b333ca3c169347
manutero <manutero@gmail.com>
2014-10-09 23:01:06 +0200

    improving geo.js. Adding different images to the markers

--- js/geo.js
+++ js/geo.js
@@ -1,39 +1,36 @@
-// geo locations (google maps)
+// geo locations (google maps & distancesfrom.com)
 // -----------------------------------
-const eindhoven_geo =   [5.45704580000006,    51.4698348];
-const utrecht_geo =     [9.4797460,           51.3127110];
-const koln_geo =        [6.95669799999996,    50.935379];
-const nueremberg_geo =  [11.074242000000027,  49.447428];
-const fuessen_geo =     [10.74980000000005,   47.55757400000001];
-const salzburg_geo =    [13.047364000000016,  47.808384];
-const brno_geo =        [16.60876499999995,   49.196995];
-const wien_geo =        [16.33069499999999,   48.194333];
-const bratislava_geo =  [17.107137299999977,  48.1458923];
-const karlovy_geo =     [12.878543499999978,  50.2414816];
-const goettingen_geo =  [9.931354699999929,   51.5309636];
-const kassel_geo =      [9.4797460,           51.3127110];
-const hamburg_geo =     [9.965321000000017,   53.549959];
+const eindhoven_geo =   [5.45704580000006, 51.469834];
+const utrecht_geo =     [5.10966489999998, 52.090142];
+const koln_geo =        [6.95669799999996, 50.935379];
+const nueremberg_geo =  [11.0742420000000, 49.447428];
+const fuessen_geo =     [10.7498000000000, 47.557574];
+const munchen_geo =     [11.5580066999999, 48.144835];
+const salzburg_geo =    [13.0473640000000, 47.808384];
+const brno_geo =        [16.6087649999999, 49.196995];
+const wien_geo =        [16.3306949999999, 48.194333];
+const bratislava_geo =  [17.1071372999999, 48.145892];
+const karlovy_geo =     [12.8785434999999, 50.241481];
+const goettingen_geo =  [9.93135469999992, 51.530963];
+const kassel_geo =      [9.47974610000005, 51.312711];
+const hamburg_geo =     [9.96532100000001, 53.549959];
 
 
-// images (imgur - @manu_tero)
+// images (imgur signed in with twitter: @manu_tero)
 // -----------------------------------
-const eindhoven_1 =   ['https://i.imgur.com/HsosQaF.jpg', 'blue flowers'];
-const eindhoven_2 =   ['', 'gaming at university'];
-
-const utrecht_1 =     ['', ];
-const utrecht_2 =     ['', ];
-
-const koln_1 =        ['https://i.imgur.com/fVKlyrw.jpg', 'The eye is watching you'];
-const koln_2 =        ['https://i.imgur.com/EeoHAGC.jpg', 'seeeah'];
-const koln_3 =        ['https://i.imgur.com/QIn4gU1.jpg', 'fer'];
-const koln_4 =        ['https://i.imgur.com/3MP29sP.jpg', ''];
-const koln_5 =        ['https://i.imgur.com/82NgbcC.jpg', ''];
-const koln_6 =        ['https://i.imgur.com/l4sXVat.jpg', ''];
-const koln_7 =        ['https://i.imgur.com/iDQ5wk6.jpg', ''];
-const koln_8 =        ['https://i.imgur.com/C9Oi6eM.jpg', ''];
-const koln_9 =        ['https://i.imgur.com/o3SJhDS.jpg', ''];
-
-const nueremberg_1 =  ['https://i.imgur.com/CRw06Wu.jpg', ''];
+const eindhoven_1 =   ['https://i.imgur.com/HsosQaF.jpg', 'Estás muy guapa con esas flores azules'];
+const eindhoven_2 =   ['https://i.imgur.com/WU6tEoM.jpg', 'Tarde de juegos en la universidad de Eindhoven'];
+const utrecht_1 =     ['https://i.imgur.com/IAXsvwn.jpg', 'Comida a la orilla del canal de Utrecht'];
+const koln_1 =        ['https://i.imgur.com/fVKlyrw.jpg', 'El ojo te observa'];
+const koln_2 =        ['https://i.imgur.com/EeoHAGC.jpg', 'Mi prima con esa actitud tan suya de seaaah'];
+const koln_3 =        ['https://i.imgur.com/QIn4gU1.jpg', 'Fer'];
+const koln_4 =        ['https://i.imgur.com/3MP29sP.jpg', 'Jimena'];
+const koln_5 =        ['https://i.imgur.com/82NgbcC.jpg', 'Que bien hablaba inglés el cabrón ¬.¬\''];
+const koln_6 =        ['https://i.imgur.com/l4sXVat.jpg', 'Mural super alegre. Pero míralos, míralos'];
+const koln_7 =        ['https://i.imgur.com/iDQ5wk6.jpg', 'La terraza de Dominich'];
+const koln_8 =        ['https://i.imgur.com/C9Oi6eM.jpg', 'Noche de futbolín en el garito Underground'];
+const koln_9 =        ['https://i.imgur.com/o3SJhDS.jpg', 'La mejor sangría que he tomado en mucho tiempo sin duda'];
+const nueremberg_1 =  ['https://i.imgur.com/CRw06Wu.jpg', 'Esta es super tierna <3'];
 const nueremberg_2 =  ['', ];
 
 const fuessen_1 =     ['https://i.imgur.com/cjFR42a.jpg', 'the bridgue - epic'];
@@ -74,7 +71,7 @@ const hamburg_1 =     ['', ];
 // images per city
 // -----------------------------------
 const eindhoven_imgs =  [eindhoven_1, eindhoven_2];
-const utrecht_imgs =    [utrecht_1, utrecht_2];
+const utrecht_imgs =    [utrecht_1];
 const koln_imgs =       [koln_1, koln_2, koln_3, koln_4, koln_5, koln_6, koln_7, koln_8, koln_9];
 const nueremberg_imgs = [nueremberg_1, nueremberg_2];
 const fuessen_imgs =    [fuessen_1, fuessen_2, fuessen_3, fuessen_4, fuessen_5, fuessen_6, fuessen_7];
@@ -87,7 +84,7 @@ const goettingen_imgs = [goettingen_1, goettingen_2, goettingen_3, goettingen_4]
 const kassel_imgs =     [kassel_1, kassel_2];
 const hamburg_imgs =    [hamburg_1];
 
-
+const markerColor = '#3c4e5a';
 var geoJson = [
 
   {
@@ -96,8 +93,8 @@ var geoJson = [
     "geometry": { "type": "Point", "coordinates": eindhoven_geo },
     "properties": {
         'title': 'Eindhoven',
-        'marker-color': '#3c4e5a',
-        'marker-symbol': 'monument',
+        'marker-color': markerColor,
+        'marker-symbol': 'bicycle',
         'marker-size': 'large',
         'images': eindhoven_imgs
     }
@@ -108,10 +105,10 @@ var geoJson = [
     type: 'Feature',
     "geometry": { "type": "Point", "coordinates": utrecht_geo },
     "properties": {
-        'title': 'Ultrecht',
-        'marker-color': '#3c4e5a',
-        'marker-symbol': 'monument',
-        'marker-size': 'large',
+        'title': 'Utrecht',
+        'marker-color': markerColor,
+        'marker-symbol': 'bakery',
+        'marker-size': 'medium',
         'images': utrecht_imgs
     }
   },
@@ -122,8 +119,8 @@ var geoJson = [
     "geometry": { "type": "Point", "coordinates": koln_geo },
     "properties": {
         'title': 'Koln',
-        'marker-color': '#3c4e5a',
-        'marker-symbol': 'monument',
+        'marker-color': markerColor,
+        'marker-symbol': 'heart',
         'marker-size': 'large',
         'images': koln_imgs
     }
@@ -135,7 +132,7 @@ var geoJson = [
     "geometry": { "type": "Point", "coordinates": nueremberg_geo },
     "properties": {
         'title': 'Nueremberg',
-        'marker-color': '#3c4e5a',
+        'marker-color': markerColor,
         'marker-symbol': 'monument',
         'marker-size': 'large',
         'images': nueremberg_imgs
@@ -148,8 +145,8 @@ var geoJson = [
     "geometry": { "type": "Point", "coordinates": fuessen_geo },
     "properties": {
         'title': 'Fuessen',
-        'marker-color': '#3c4e5a',
-        'marker-symbol': 'monument',
+        'marker-color': markerColor,
+        'marker-symbol': 'camera',
         'marker-size': 'large',
         'images': fuessen_imgs
     }
@@ -161,10 +158,23 @@ var geoJson = [
     "geometry": { "type": "Point", "coordinates": salzburg_geo },
     "properties": {
         'title': 'Salzburg',
-        'marker-color': '#3c4e5a',
-        'marker-symbol': 'monument',
+        'marker-color': markerColor,
+        'marker-symbol': 'music',
         'marker-size': 'large',
         'images': salzburg_imgs
+    }
+  },
+
+  {
+    // Brno
+    type: 'Feature',
+    "geometry": { "type": "Point", "coordinates": brno_geo },
+    "properties": {
+        'title': 'Brno',
+        'marker-color': markerColor,
+        'marker-symbol': 'music',
+        'marker-size': 'large',
+        'images': brno_imgs
     }
   },
 
@@ -174,7 +184,7 @@ var geoJson = [
     "geometry": { "type": "Point", "coordinates": wien_geo },
     "properties": {
         'title': 'Wien',
-        'marker-color': '#3c4e5a',
+        'marker-color': markerColor,
         'marker-symbol': 'monument',
         'marker-size': 'large',
         'images': wien_imgs
@@ -187,23 +197,10 @@ var geoJson = [
     "geometry": { "type": "Point", "coordinates": bratislava_geo },
     "properties": {
         'title': 'Bratislava',
-        'marker-color': '#3c4e5a',
-        'marker-symbol': 'monument',
-        'marker-size': 'large',
+        'marker-color': markerColor,
+        'marker-symbol': 'bakery',
+        'marker-size': 'medium',
         'images': bratislava_imgs
-    }
-  },
-
-  {
-    // Brno
-    type: 'Feature',
-    "geometry": { "type": "Point", "coordinates": brno_geo },
-    "properties": {
-        'title': 'Brno',
-        'marker-color': '#3c4e5a',
-        'marker-symbol': 'monument',
-        'marker-size': 'large',
-        'images': brno_imgs
     }
   },
 
@@ -213,8 +210,8 @@ var geoJson = [
     "geometry": { "type": "Point", "coordinates": karlovy_geo },
     "properties": {
         'title': 'Karlovy Vary',
-        'marker-color': '#3c4e5a',
-        'marker-symbol': 'monument',
+        'marker-color': markerColor,
+        'marker-symbol': 'park',
         'marker-size': 'large',
         'images': karlovy_imgs
     }
@@ -226,8 +223,8 @@ var geoJson = [
     "geometry": { "type": "Point", "coordinates": goettingen_geo },
     "properties": {
         'title': 'Goëttingen',
-        'marker-color': '#3c4e5a',
-        'marker-symbol': 'monument',
+        'marker-color': markerColor,
+        'marker-symbol': 'college',
         'marker-size': 'large',
         'images': goettingen_imgs
     }
@@ -239,9 +236,9 @@ var geoJson = [
     "geometry": { "type": "Point", "coordinates": kassel_geo },
     "properties": {
         'title': 'Kassel',
-        'marker-color': '#3c4e5a',
-        'marker-symbol': 'monument',
-        'marker-size': 'large',
+        'marker-color': markerColor,
+        'marker-symbol': 'park',
+        'marker-size': 'medium',
         'images': kassel_imgs
     }
   },
@@ -252,8 +249,8 @@ var geoJson = [
     "geometry": { "type": "Point", "coordinates": hamburg_geo },
     "properties": {
         'title': 'Hamburg',
-        'marker-color': '#3c4e5a',
-        'marker-symbol': 'monument',
+        'marker-color': markerColor,
+        'marker-symbol': 'airport',
         'marker-size': 'large',
         'images': hamburg_imgs
     }
