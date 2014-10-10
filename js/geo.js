305d49572c15cc33196e91884c1cbb3e1a83e8b1
manutero <manutero@gmail.com>
2014-10-10 12:04:58 +0200

    finished geo.js at 99%

--- js/geo.js
+++ js/geo.js
@@ -20,236 +20,291 @@ const hamburg_geo =     [9.96532100000001, 53.549959];
 // -----------------------------------
 const eindhoven_1 =   ['https://i.imgur.com/HsosQaF.jpg', 'Estás muy guapa con esas flores azules'];
 const eindhoven_2 =   ['https://i.imgur.com/WU6tEoM.jpg', 'Tarde de juegos en la universidad de Eindhoven'];
+const eindhoven_3 =   ['https://i.imgur.com/6T7SaUv.jpg'];
+const eindhoven_4 =   ['https://i.imgur.com/OO28K30.jpg'];
 const utrecht_1 =     ['https://i.imgur.com/IAXsvwn.jpg', 'Comida a la orilla del canal de Utrecht'];
-const koln_1 =        ['https://i.imgur.com/fVKlyrw.jpg', 'El ojo te observa'];
+const koln_1 =        ['https://i.imgur.com/fVKlyrw.jpg', 'El ojo te observa xD'];
 const koln_2 =        ['https://i.imgur.com/EeoHAGC.jpg', 'Mi prima con esa actitud tan suya de seaaah'];
 const koln_3 =        ['https://i.imgur.com/QIn4gU1.jpg', 'Fer'];
 const koln_4 =        ['https://i.imgur.com/3MP29sP.jpg', 'Jimena'];
 const koln_5 =        ['https://i.imgur.com/82NgbcC.jpg', 'Que bien hablaba inglés el cabrón ¬.¬\''];
-const koln_6 =        ['https://i.imgur.com/l4sXVat.jpg', 'Mural super alegre. Pero míralos, míralos'];
-const koln_7 =        ['https://i.imgur.com/iDQ5wk6.jpg', 'La terraza de Dominich'];
+const koln_6 =        ['https://i.imgur.com/l4sXVat.jpg'];
+const koln_7 =        ['https://i.imgur.com/wZQCL5v.jpg', 'Las chicas'];
 const koln_8 =        ['https://i.imgur.com/C9Oi6eM.jpg', 'Noche de futbolín en el garito Underground'];
 const koln_9 =        ['https://i.imgur.com/o3SJhDS.jpg', 'La mejor sangría que he tomado en mucho tiempo sin duda'];
+const koln_10 =       ['https://i.imgur.com/lgGTAel.jpg'];
+const koln_11 =       ['https://i.imgur.com/iDQ5wk6.jpg', 'La terraza de Dominich'];
+const koln_12 =       ['https://i.imgur.com/TkY1JpV.jpg', 'Un hombre superado por su abrigo'];
+const koln_13 =       ['https://i.imgur.com/UZWtW1k.jpg'];
 const nueremberg_1 =  ['https://i.imgur.com/CRw06Wu.jpg', 'Esta es super tierna <3'];
-const nueremberg_2 =  ['', ];
-
-const fuessen_1 =     ['https://i.imgur.com/cjFR42a.jpg', 'the bridgue - epic'];
-const fuessen_2 =     ['https://i.imgur.com/fWEXdPD.jpg', 'croquis'];
-const fuessen_3 =     ['https://i.imgur.com/M43ztiq.jpg', 'the bridgue - Yiwen'];
-const fuessen_4 =     ['https://i.imgur.com/6lqohY8.jpg', 'the bridgue - ¿?'];
-const fuessen_5 =     ['https://i.imgur.com/b3EgST8.jpg', 'the bridgue - mym'];
+const nueremberg_2 =  ['https://i.imgur.com/bEC87mk.jpg'];
+const nueremberg_3 =  ['https://i.imgur.com/drZfh1G.jpg', 'El castillo de Nüeremberg'];
+const nueremberg_4 =  ['https://i.imgur.com/XDnS31L.jpg'];
+const fuessen_1 =     ['https://i.imgur.com/cjFR42a.jpg', 'LA Foto (with capital F)'];
+const fuessen_2 =     ['https://i.imgur.com/fWEXdPD.jpg', 'La Foto - croquis'];
+const fuessen_3 =     ['https://i.imgur.com/M43ztiq.jpg', 'El puente - Yiwen'];
+const fuessen_4 =     ['https://i.imgur.com/6lqohY8.jpg', 'El puente - Yin Li'];
+const fuessen_5 =     ['https://i.imgur.com/b3EgST8.jpg', 'El puente - nosotros'];
 const fuessen_6 =     ['https://i.imgur.com/ELn2cYa.jpg', 'incognito'];
 const fuessen_7 =     ['https://i.imgur.com/MIxLj1F.jpg', 'the lake'];
-
-const salzburg_1 =    ['https://i.imgur.com/RkKc9qB.jpg', ''];
-const salzburg_2 =    ['https://i.imgur.com/u1xYjgh.jpg', 'The kiss'];
-const salzburg_3 =    ['', ];
-
+const fuessen_8 =     ['https://i.imgur.com/qyTtB8Z.jpg'];
+const fuessen_9 =     ['https://i.imgur.com/kq6T96E.jpg'];
+const fuessen_10 =    ['https://i.imgur.com/cANFOoH.jpg'];
+const salzburg_1 =    ['https://i.imgur.com/RkKc9qB.jpg', 'Compañeros portugueses'];
+const salzburg_2 =    ['https://i.imgur.com/u1xYjgh.jpg', 'The kiss (el de verdad)'];
+const salzburg_3 =    ['https://i.imgur.com/MVDh3BH.jpg', 'El hombre arbóreo'];
+const salzburg_4 =    ['https://i.imgur.com/jFUjk80.jpg', 'Ella'];
+const salzburg_5 =    ['https://i.imgur.com/TQrQWaa.jpg', 'Subnormal nivel 2'];
+const salzburg_6 =    ['https://i.imgur.com/k03YK4X.jpg', 'Antes...'];
+const salzburg_7 =    ['https://i.imgur.com/xWtdpCx.jpg', '... Y después'];
+const salzburg_8 =    ['https://i.imgur.com/z0frEaw.jpg', 'Entrando a un concierto inesperado'];
+const salzburg_9 =    ['https://i.imgur.com/nuHlgQ2.jpg', 'La ciudad donde nació Mozart'];
+const salzburg_10 =   ['https://i.imgur.com/OyI5GNU.jpg', 'Corean music'];
+const salzburg_11 =   ['https://i.imgur.com/UsZgYaI.jpg', 'Uggh'];
+const salzburg_12 =   ['https://i.imgur.com/rtv7NKB.jpg'];
+const salzburg_13 =   ['https://i.imgur.com/HccNu0x.jpg', 'Erótico resultado'];
 const brno_1 =        ['https://i.imgur.com/X1FfeLQ.jpg', 'what\'s this'];
-const brno_2 =        ['https://i.imgur.com/4GKUNhF.jpg', 'hear our rorar'];
-const brno_3 =        ['https://i.imgur.com/LxCr11F.jpg', 'live concert'];
-
-const wien_1 =        ['https://i.imgur.com/ci8Phjv.jpg', ''];
-const wien_2 =        ['', ];
-
+const brno_2 =        ['https://i.imgur.com/4GKUNhF.jpg', '¡Oye nuestro rugido!'];
+const brno_3 =        ['https://i.imgur.com/LxCr11F.jpg', 'Concierto hispano-checo en Brno'];
+const brno_4 =        ['https://i.imgur.com/WhGtgYF.jpg', 'Estás super curiosa'];
+const brno_5 =        ['https://i.imgur.com/AHBYVa2.jpg', 'Y aquí super concentrada'];
+const brno_6 =        ['https://i.imgur.com/AZwaKne.jpg'];
+const brno_7 =        ['https://i.imgur.com/6MIMteD.jpg'];
+const brno_8 =        ['https://i.imgur.com/8wnUqyZ.jpg'];
+const brno_9 =        ['https://i.imgur.com/zPgNydH.jpg'];
+const brno_10 =       ['https://i.imgur.com/lQyfdxJ.jpg'];
+const brno_11 =       ['https://i.imgur.com/icYcs2b.jpg'];
+const brno_12 =       ['https://i.imgur.com/O1krJWc.jpg'];
+const wien_1 =        ['https://i.imgur.com/ci8Phjv.jpg'];
+const wien_2 =        ['https://i.imgur.com/V1rCx6s.jpg'];
+const wien_3 =        ['https://i.imgur.com/OR2UaYp.jpg'];
+const wien_4 =        ['https://i.imgur.com/UZ3n1kk.jpg'];
+const wien_5 =        ['https://i.imgur.com/CXQsKvn.jpg'];
+const wien_6 =        ['https://i.imgur.com/HpxaLAg.jpg'];
+const wien_7 =        ['https://i.imgur.com/JRavQpy.jpg'];
+const wien_8 =        ['https://i.imgur.com/UBWQWdz.jpg'];
+const wien_9 =        ['https://i.imgur.com/r7wyxq4.jpg'];
+const wien_10 =       ['https://i.imgur.com/gCxHcjL.jpg'];
+const wien_11 =       ['https://i.imgur.com/LcHVFzK.jpg'];
+const wien_12 =       ['https://i.imgur.com/w8GaLaC.jpg'];
+const wien_13 =       ['https://i.imgur.com/LlQdjg7.jpg'];
 const bratislava_1 =  ['https://i.imgur.com/8fMN2hU.jpg', 'waiting for'];
-
+const bratislava_2 =  ['https://i.imgur.com/GQIZ8gN.jpg'];
+const bratislava_3 =  ['https://i.imgur.com/8aC8bXB.jpg'];
 const karlovy_1 =     ['https://i.imgur.com/kKtB8c6.jpg', 'Gabrilea'];
 const karlovy_2 =     ['https://i.imgur.com/yKP9Vcv.jpg', 'colored houses'];
-
+const karlovy_3 =     ['https://i.imgur.com/OlAotT6.jpg'];
+const karlovy_4 =     ['https://i.imgur.com/hyFNeWz.jpg'];
+const karlovy_5 =     ['https://i.imgur.com/frMIQHV.jpg'];
+const karlovy_6 =     ['https://i.imgur.com/NIHs29W.jpg'];
+const karlovy_7 =     ['https://i.imgur.com/6YIS774.jpg'];
+const karlovy_8 =     ['https://i.imgur.com/Xoxsve7.jpg'];
+const karlovy_9 =     ['https://i.imgur.com/uStOrIW.jpg'];
 const goettingen_1 =  ['https://i.imgur.com/1CPj02q.jpg', 'the fantastic 4'];
 const goettingen_2 =  ['https://i.imgur.com/L9AV5si.jpg', 'alex'];
 const goettingen_3 =  ['https://i.imgur.com/OUvAaEk.jpg', 'fer'];
 const goettingen_4 =  ['https://i.imgur.com/SUBlA4n.jpg', 'from hell'];
-
+const goettingen_6 =  ['https://i.imgur.com/WkSUbqd.jpg'];
+const goettingen_7 =  ['https://i.imgur.com/Pi6V8ou.jpg'];
+const goettingen_8 =  ['https://i.imgur.com/L5AndKG.jpg'];
 const kassel_1 =      ['https://i.imgur.com/Q4cSb79.jpg', 'hich selfhi']
 const kassel_2 =      ['https://i.imgur.com/7AsSnnY.jpg', 'herculus ass'];
-
-const hamburg_1 =     ['', ];
-
+const kassel_3 =      ['https://i.imgur.com/HGAOxGm.jpg'];
+const kassel_4 =      ['https://i.imgur.com/RsLR1lt.jpg'];
+const kassel_5 =  ['https://i.imgur.com/m5NLKZs.jpg'];
+const hamburg_1 =     ['https://i.imgur.com/EjBquvv.jpg'];
+const hamburg_2 =     ['https://i.imgur.com/zinPc8H.png'];
+const hamburg_3 =     ['https://i.imgur.com/VYskMAe.jpg'];
+const hamburg_4 =     ['https://i.imgur.com/aVpzQ6C.jpg'];
+const hamburg_5 =     ['https://i.imgur.com/3FX2mZ9.png'];
+const hamburg_6 =     ['https://i.imgur.com/6M6HO8t.png'];
+const hamburg_7 =     ['https://i.imgur.com/JgORGyO.png'];
 
 // images per city
 // -----------------------------------
-const eindhoven_imgs =  [eindhoven_1, eindhoven_2];
-const utrecht_imgs =    [utrecht_1];
-const koln_imgs =       [koln_1, koln_2, koln_3, koln_4, koln_5, koln_6, koln_7, koln_8, koln_9];
-const nueremberg_imgs = [nueremberg_1, nueremberg_2];
-const fuessen_imgs =    [fuessen_1, fuessen_2, fuessen_3, fuessen_4, fuessen_5, fuessen_6, fuessen_7];
-const salzburg_imgs =   [salzburg_1, salzburg_2, salzburg_3];
-const brno_imgs =       [brno_1, brno_2, brno_3];
-const wien_imgs =       [wien_1, wien_2];
-const bratislava_imgs = [bratislava_1];
-const karlovy_imgs =    [karlovy_1, karlovy_2];
-const goettingen_imgs = [goettingen_1, goettingen_2, goettingen_3, goettingen_4];
-const kassel_imgs =     [kassel_1, kassel_2];
-const hamburg_imgs =    [hamburg_1];
+const eindhoven_imgs =
+  [eindhoven_1, eindhoven_2, eindhoven_3, eindhoven_4];
+const utrecht_imgs =
+  [utrecht_1];
+const koln_imgs =
+  [koln_1, koln_2, koln_3, koln_4, koln_5, koln_6, koln_7, koln_8, koln_9, koln_10, koln_11, koln_12, koln_13];
+const nueremberg_imgs =
+  [nueremberg_1, nueremberg_2, nueremberg_3, nueremberg_4];
+const fuessen_imgs =
+  [fuessen_1, fuessen_2, fuessen_3, fuessen_4, fuessen_5, fuessen_6, fuessen_7, fuessen_8, fuessen_9, fuessen_10];
+const salzburg_imgs =
+  [salzburg_1, salzburg_2, salzburg_3, salzburg_4, salzburg_5, salzburg_6, salzburg_7, salzburg_8, salzburg_9, salzburg_10, salzburg_11, salzburg_12, salzburg_13];
+const brno_imgs =
+  [brno_1, brno_2, brno_3, brno_4, brno_5, brno_6, brno_7, brno_8, brno_9, brno_10, brno_11, brno_12];
+const wien_imgs =
+  [wien_1, wien_2, wien_3, wien_4, wien_5, wien_6, wien_7, wien_8, wien_9, wien_10, wien_11, wien_12, wien_13];
+const bratislava_imgs =
+  [bratislava_1, bratislava_2, bratislava_3];
+const karlovy_imgs =
+  [karlovy_1, karlovy_2, karlovy_3, karlovy_4, karlovy_5, karlovy_6, karlovy_7, karlovy_8, karlovy_9];
+const goettingen_imgs =
+  [goettingen_1, goettingen_2, goettingen_3, goettingen_4, goettingen_6, goettingen_7, goettingen_8];
+const kassel_imgs =
+  [kassel_1, kassel_2, kassel_3, kassel_4, kassel_5];
+const hamburg_imgs =
+  [hamburg_1, hamburg_2, hamburg_3, hamburg_4, hamburg_5, hamburg_6, hamburg_7];
 
-const markerColor = '#3c4e5a';
+
+// geoJson
+// -----------------------------------
+const MARKER_COLOR = '#3c4e5a';
 var geoJson = [
-
   {
     // Eindhoven
     type: 'Feature',
     "geometry": { "type": "Point", "coordinates": eindhoven_geo },
     "properties": {
         'title': 'Eindhoven',
-        'marker-color': markerColor,
+        'marker-color': MARKER_COLOR,
         'marker-symbol': 'bicycle',
         'marker-size': 'large',
         'images': eindhoven_imgs
     }
   },
-
   {
     // Utrecht
     type: 'Feature',
     "geometry": { "type": "Point", "coordinates": utrecht_geo },
     "properties": {
         'title': 'Utrecht',
-        'marker-color': markerColor,
+        'marker-color': MARKER_COLOR,
         'marker-symbol': 'bakery',
         'marker-size': 'medium',
         'images': utrecht_imgs
     }
   },
-
   {
     // Koln
     type: 'Feature',
     "geometry": { "type": "Point", "coordinates": koln_geo },
     "properties": {
         'title': 'Koln',
-        'marker-color': markerColor,
+        'marker-color': MARKER_COLOR,
         'marker-symbol': 'heart',
         'marker-size': 'large',
         'images': koln_imgs
     }
   },
-
   {
     // Nueremberg
     type: 'Feature',
     "geometry": { "type": "Point", "coordinates": nueremberg_geo },
     "properties": {
         'title': 'Nueremberg',
-        'marker-color': markerColor,
+        'marker-color': MARKER_COLOR,
         'marker-symbol': 'monument',
         'marker-size': 'large',
         'images': nueremberg_imgs
     }
   },
-
   {
     // Fuessen
     type: 'Feature',
     "geometry": { "type": "Point", "coordinates": fuessen_geo },
     "properties": {
         'title': 'Fuessen',
-        'marker-color': markerColor,
+        'marker-color': MARKER_COLOR,
         'marker-symbol': 'camera',
         'marker-size': 'large',
         'images': fuessen_imgs
     }
   },
-
   {
     // Salzburg
     type: 'Feature',
     "geometry": { "type": "Point", "coordinates": salzburg_geo },
     "properties": {
         'title': 'Salzburg',
-        'marker-color': markerColor,
+        'marker-color': MARKER_COLOR,
         'marker-symbol': 'music',
         'marker-size': 'large',
         'images': salzburg_imgs
     }
   },
-
   {
     // Brno
     type: 'Feature',
     "geometry": { "type": "Point", "coordinates": brno_geo },
     "properties": {
         'title': 'Brno',
-        'marker-color': markerColor,
+        'marker-color': MARKER_COLOR,
         'marker-symbol': 'music',
         'marker-size': 'large',
         'images': brno_imgs
     }
   },
-
   {
     // Wien
     type: 'Feature',
     "geometry": { "type": "Point", "coordinates": wien_geo },
     "properties": {
         'title': 'Wien',
-        'marker-color': markerColor,
+        'marker-color': MARKER_COLOR,
         'marker-symbol': 'monument',
         'marker-size': 'large',
         'images': wien_imgs
     }
   },
-
   {
     // Bratislava
     type: 'Feature',
     "geometry": { "type": "Point", "coordinates": bratislava_geo },
     "properties": {
         'title': 'Bratislava',
-        'marker-color': markerColor,
+        'marker-color': MARKER_COLOR,
         'marker-symbol': 'bakery',
         'marker-size': 'medium',
         'images': bratislava_imgs
     }
   },
-
   {
     // Karlovy
     type: 'Feature',
     "geometry": { "type": "Point", "coordinates": karlovy_geo },
     "properties": {
         'title': 'Karlovy Vary',
-        'marker-color': markerColor,
+        'marker-color': MARKER_COLOR,
         'marker-symbol': 'park',
         'marker-size': 'large',
         'images': karlovy_imgs
     }
   },
-
   {
     // Goettingen
     type: 'Feature',
     "geometry": { "type": "Point", "coordinates": goettingen_geo },
     "properties": {
         'title': 'Goëttingen',
-        'marker-color': markerColor,
+        'marker-color': MARKER_COLOR,
         'marker-symbol': 'college',
         'marker-size': 'large',
         'images': goettingen_imgs
     }
   },
-
   {
     // Kassel
     type: 'Feature',
     "geometry": { "type": "Point", "coordinates": kassel_geo },
     "properties": {
         'title': 'Kassel',
-        'marker-color': markerColor,
+        'marker-color': MARKER_COLOR,
         'marker-symbol': 'park',
         'marker-size': 'medium',
         'images': kassel_imgs
     }
   },
-
   {
     // Hamburg
     type: 'Feature',
     "geometry": { "type": "Point", "coordinates": hamburg_geo },
     "properties": {
         'title': 'Hamburg',
-        'marker-color': markerColor,
+        'marker-color': MARKER_COLOR,
         'marker-symbol': 'airport',
         'marker-size': 'large',
         'images': hamburg_imgs
